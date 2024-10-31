bc98a4560f96261938734c0119a7daf224acfe91
Joe Hildebrand <joe-github@cursive.net>
2024-10-31 01:36:45 -0600

    Allow sending extra headers.
    This might be useful for COOP and COEP headers,
    for instance.

--- .hostlocal.js
+++ .hostlocal.js
@@ -1,6 +1,13 @@
+import {version} from './lib/version.js';
+
 export default {
   port: 8112,
   open: 'docs/index.html',
   glob: ['src/*.ts', 'README.md'],
   exec: 'npm run docs',
+  headers: {
+    'x-hostlocal-version': version,
+    'Cross-Origin-Opener-Policy': 'same-origin',
+    'Cross-Origin-Embedder-Policy': 'require-corp',
+  },
 };
